ed9d6800d8353babb550cd5c81380ec55adffcf6
rumyunus <101033616+rumyunus@users.noreply.github.com>
2023-04-12 13:16:14 +0530

    Update HelloWorldServlet.java

--- HelloWorldServlet.java
+++ HelloWorldServlet.java
@@ -4,17 +4,16 @@ import java.io.*;
 
 public class HelloWorldServlet extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
-            throws ServletException, IOException {
-                    response.setContentType("text/html");
-                            PrintWriter out = response.getWriter();
-                                    out.println("<html>");
-                                            out.println("<head>");
-                                                    out.println("<title>Hello World Servlet</title>");
-                                                            out.println("</head>");
-                                                                    out.println("<body>");
-                                                                            out.println("<h1>Hello World from a Java Servlet!</h1>");
-                                                                                    out.println("</body>");
-                                                                                            out.println("</html>");
-                                                                                                }
-                                                                                                }
-
+        throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        out.println("<html>");
+        out.println("<head>");
+        out.println("<title>Hello World Servlet</title>");
+        out.println("</head>");
+        out.println("<body>");
+        out.println("<h1>Hello World from a Java Servlet!</h1>");
+        out.println("</body>");
+        out.println("</html>");
+    }
+}
